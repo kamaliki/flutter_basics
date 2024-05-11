a8c649b012945e7fe97fc568a7edd5868fe4d6ad
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2024-05-12 00:55:11 +0300

    homescreen

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/utils/colors.dart';
+import 'package:flutter_basics/utils/global_variables.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -10,9 +11,29 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
   @override
   void initState() {
     super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
   }
 
   @override
@@ -22,40 +43,57 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
         appBar: AppBar(
           title: const Text('Mobile Screen Layout'),
         ),
-        body: const Center(
-          child: Text(
-            'Welcome ',
-            style: TextStyle(fontSize: 24),
-          ),
+        body: PageView(
+          children: homeScreenItems
+          physics:  const NeverScrollableScrollPhysics(),
+          controller: pageController,
+          onPageChanged: onPageChanged,
         ),
         bottomNavigationBar: CupertinoTabBar(
           backgroundColor: mobileBackgroundColor,
-          items: const <BottomNavigationBarItem>[
+          items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-                backgroundColor: primaryColor),
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: 'Home',
+              backgroundColor: primaryColor,
+            ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.search),
+              icon: Icon(
+                Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
               label: 'Search',
               backgroundColor: primaryColor,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.add_circle),
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
               label: 'Post',
               backgroundColor: primaryColor,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
               label: 'Notifications',
               backgroundColor: primaryColor,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.person),
+              icon: Icon(
+                Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor,
+              ),
               backgroundColor: primaryColor,
               label: 'Profile',
             ),
           ],
+          onTap: navigationTapped,
           //selectedItemColor: blueColor,
         ));
   }
