fcfdf267ef371d24b68ac1f13bec29cf88eceac0
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2024-05-11 21:39:33 +0300

    Update mobile_screen_layout.dart
    Bottom navigation

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,56 +1,62 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basics/providers/user_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_basics/models/user.dart' as model;
-
+import 'package:flutter_basics/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
-
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String username = '';
-
   @override
   void initState() {
-    super.initState
-    ();
-    getUsername();
+    super.initState();
   }
-
-  void getUsername() async{
-    DocumentSnapshot snap = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .get();
-
-    setState(() {
-      username = (snap.data() as Map<String, dynamic>)['username'];
-    });
-    }
-
 
   @override
   Widget build(BuildContext context) {
     //model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mobile Screen Layout'),
-      ),
-      body: Center(
-        child: Text(
-          'Welcome ',
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          title: const Text('Mobile Screen Layout'),
         ),
-      ),
-    );
+        body: const Center(
+          child: Text(
+            'Welcome ',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: mobileBackgroundColor,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: primaryColor),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle),
+              label: 'Post',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Notifications',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              backgroundColor: primaryColor,
+              label: 'Profile',
+            ),
+          ],
+          //selectedItemColor: blueColor,
+        ));
   }
-
-
 }
