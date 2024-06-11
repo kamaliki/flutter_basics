6def380190d3a1c7c0834c498cc7811c04c8978a
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2024-06-11 22:15:07 +0300

    wow, screens

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/screens/add_post_screen.dart';
 
+import '../screens/feed_screen.dart';
+
     const webScreenSize = 600;
 
     const homeScreenItems = [
-        Text('Home'),
+        FeedScreen(),
         Text('Search'),
         AddPostScreen(),
         Text('Notifications'),
